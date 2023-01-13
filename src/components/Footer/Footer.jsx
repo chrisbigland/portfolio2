fcfd142e23e177d66ae4ecf48901d16d0798656e
CHRIS BIGLAND <christopherbigland@gmail.com>
2023-01-13 16:43:51 +0000

    footer stylings added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../../abstracts/globalStyles.scss'
+import styles from './Footer.module.scss'
 
 const Footer = () => {
   return (
-    <div class="footer">
-        <span class="footer__content">christopherbigland@gmail.com</span>
-        <span class="footer__content">All Rights Reserved. 2022.</span>
-        <div class="footer__icons">
-            <a class="footer__link" href="#"><img class="footer__content-icon" src="./images/cv-icon.svg" alt="CV icon"></img></a>
-            <a class="footer__link" href="#"><img class="footer__content-icon" src="./images/github-icon.svg" alt="GitHub icon"></img></a>
-            <a class="footer__link" href="#"><img class="footer__content-icon" src="./images/linkedin-icon.svg" alt="LinkedIn icon"></img></a>
+    <div className={styles.Footer}>
+        <span className={styles.Footer_content}>christopherbigland@gmail.com</span>
+        <span className={styles.Footer_content}>All Rights Reserved. 2022.</span>
+        <div className={styles.Footer_icons}>
+            <a className={styles.Footer_link} href="#"><img className={styles.Footer_contentIcon} src="./images/cv-icon.svg" alt="CV icon"></img></a>
+            <a className={styles.Footer_link} href="#"><img className={styles.Footer_contentIcon} src="./images/github-icon.svg" alt="GitHub icon"></img></a>
+            <a className={styles.Footer_link} href="#"><img className={styles.Footer_contentIcon} src="./images/linkedin-icon.svg" alt="LinkedIn icon"></img></a>
         </div>
     </div>
   )
